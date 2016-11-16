e3be459dfb9121d8bca01cc22ad5b803ca806be8
GeeksdelaCave <geeksdelacave@laposte.net>
2016-11-16 17:17:06 +0100

    No idea of the last edit

--- src/main/java/fr/univ_amu/iut/GestionExpeditions.java
+++ src/main/java/fr/univ_amu/iut/GestionExpeditions.java
@@ -3,7 +3,7 @@ package fr.univ_amu.iut;
 public class GestionExpeditions {
 
     public static void main(String args[]) {
-        System.out.println("******** CHARGEMENT VERS LA TUNISIE ! :D ********");
+        System.out.println("******** CHARGEMENT VERS LA TUNISIE ! :D ********" + '\n');
         for (Conteneur conteneur : Lecteur.charger("monFichier")) {
             System.out.println("Conteneur chargé ! (distance " + conteneur.getDistance() + ") || " + conteneur);
         }
